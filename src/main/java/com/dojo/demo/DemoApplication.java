aa00e0e2805e4ca67f85c9b829a0749e96af486c
bberton <bberton@ciandt.com>
2019-04-06 16:01:29 -0300

    Adjust for dojo

--- src/main/java/com/dojo/demo/DemoApplication.java
+++ src/main/java/com/dojo/demo/DemoApplication.java
@@ -1,11 +1,8 @@
 package com.dojo.demo;
 
-import com.dojo.demo.service.properties.DummyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties({ DummyProperties.class })
 @SpringBootApplication
 public class DemoApplication {
 
